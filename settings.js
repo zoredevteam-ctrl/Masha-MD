340fc576ad831186349f0a979f9ecbd577231344
˚₊· ͟͟͞͞  ɪ ᴀᴍ ᴋᴀᴍᴇᴋɪ <zoredevteam@gmail.com>
2026-05-05 09:28:20 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.rcanal         = 'https://whatsapp.com/channel/0029Vb85bh7EAKWOM4Zw8N3G'
 global.newsletterJid  = '120363408182996815@newsletter'
 global.newsletterName = '「✦ 𝐌𝐚𝐬𝐡𝐚 𝐊𝐮𝐣𝐨𝐮 ✦」'
 
-global.banner = 'https://i.imgur.com/placeholder-banner.jpg'
+global.banner = 'https://causas-files.vercel.app/fl/hc2x.webp'
 global.icono  = 'https://i.imgur.com/placeholder-icon.jpg'
 
 // ══════════════════════════════════════════
